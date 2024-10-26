13f9df9bc349655f0b3b27dac0df4302ff3778aa
4-alokk <alokkumar.business@gmail.com>
2024-10-26 09:07:49 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './features/counter/counterSlice';
+
+
+function App() {
+
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
